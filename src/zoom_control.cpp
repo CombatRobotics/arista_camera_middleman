7b390d8061e0c95cc7f994702353a2178f94980a
Harsh Davda <harsh.davda11@outlook.com>
2025-04-21 19:09:32 +0530

    preset mode shift

--- src/zoom_control.cpp
+++ src/zoom_control.cpp
@@ -7,7 +7,7 @@
 #include <string>
 
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/u_int16.hpp"
+#include "std_msgs/msg/bool.hpp"
 #include "arista_camera_middleman/CamFocuser.hpp"
 #include <termios.h>
 #include <unistd.h>
@@ -34,24 +34,37 @@ class ZoomPresetSubscriber : public rclcpp::Node
 public:
   ZoomPresetSubscriber() : Node("zoom_presets_subscriber"),focuser(RPI_BUS_ID)
   {
-    subscription_ = this->create_subscription<std_msgs::msg::UInt16>(
-      "preset_idx", 10, [this](const std_msgs::msg::UInt16::SharedPtr msg) { this->zoomPresetCallback(msg); });
+    subscription_ = this->create_subscription<std_msgs::msg::Bool>(
+      "preset_idx", 10, [this](const std_msgs::msg::Bool::SharedPtr msg) { this->zoomPresetCallback(msg); });
   };
 
 
   inline void set_preset(uint32_t preset_index){
-    preset_index = preset_index % NUM_PRESETS;
+    this->preset_index = preset_index;
     const ZoomPreset& preset = presets[preset_index];
     focuser.set(Focuser::OPT_ZOOM, preset.zoom);
     focuser.set(Focuser::OPT_FOCUS, preset.focus);
   };
 
 private:
-void zoomPresetCallback(const std_msgs::msg::UInt16::SharedPtr msg){
-    set_preset(msg->data);
+void zoomPresetCallback(const std_msgs::msg::Bool::SharedPtr msg){
+  if (msg->data)
+  {
+    if(preset_index < NUM_PRESETS - 1){
+      preset_index++;
+      this->set_preset(preset_index);
+    }
+  } else {
+    if(preset_index > 0){
+      preset_index--;
+      this->set_preset(preset_index);
+    }
+  }
+  
 };
-  rclcpp::Subscription<std_msgs::msg::UInt16>::SharedPtr subscription_;
+  rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr subscription_;
   Focuser focuser;
+  uint16_t preset_index;
 };
 
 int main(int argc, char * argv[])
