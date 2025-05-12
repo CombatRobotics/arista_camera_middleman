78983ad7d5599e0f85b361d09968116ff9fa7a03
Naman Malik <naman@comabtrobotics.in>
2025-05-12 19:32:35 +0530

    minor fix

--- launch/middleman.launch.py
+++ launch/middleman.launch.py
@@ -89,14 +89,14 @@ def make_launch_desc(robot_conf:AvailableRobot):
         output='screen',
         respawn=True
     )
-    # zoom_control_node = Node(
-    #     package='arista_camera_middleman',
-    #     executable='zoom_control',
-    #     name='zoom_control',
-    #     namespace=namespace,
-    #     output='screen',
-    #     respawn=True
-    # )
+    zoom_control_node = Node(
+        package='arista_camera_middleman',
+        executable='zoom_control',
+        name='zoom_control',
+        namespace=namespace,
+        output='screen',
+        respawn=True
+    )
     thermal_cam_stream = Node(
         package='arista_video_stream',
         executable='rpicam_stream.py',
@@ -125,7 +125,7 @@ def make_launch_desc(robot_conf:AvailableRobot):
     )
     return launch.LaunchDescription([
         can_control_node,
-        zoom_control_node,
+        # zoom_control_node,
         thermal_cam_stream,
         rgb_cam_stream,
     ])
